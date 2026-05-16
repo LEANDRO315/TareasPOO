2aa0355bf07b4db98304701f50a6b9b384ec8e17
LEANDRO315 <10235@itescam.edu.mx>
2026-05-16 02:10:12 -0600

    Avance de la Practica 6

---
+++ PRACTICA 6.POLIMORFISMO/FiguraGeometrica.java
@@ -0,0 +1,13 @@
+public abstract class FiguraGeometrica {
+
+    // Metodos abstractos
+    abstract void dibujar();
+    //codigo para dibujar la figura geometrica
+
+    abstract double calcularArea();
+    //codigo para calcular el área correspondiente a la figura
+
+    abstract double calcularPerimetro();
+    //codigo para calcular el perímetro correspondiente a la figura
+
+}
